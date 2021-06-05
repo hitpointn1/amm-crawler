994c783c492564b2b1e9c054c7aba2cc45c44acf
Nickolay <hitpoin1@gmail.com>
2021-06-05 15:39:55 +0300

    API for graph data presentaion

---
+++ AMMCrawler.API/Controllers/GraphController.cs
@@ -0,0 +1,32 @@
+﻿using AMMCrawler.ServiceLayer.Abstractions;
+using AMMCrawler.ServiceLayer.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AMMCrawler.API.Controllers
+{
+    [Route("/api/{controller}/{action}")]
+    public class GraphController : Controller
+    {
+        private readonly IApplicationService applicationService;
+        private readonly IGraphService graphService;
+
+        public GraphController(IApplicationService applicationService, IGraphService graphService)
+        {
+            this.applicationService = applicationService;
+            this.graphService = graphService;
+        }
+
+        [HttpGet]
+        public Task<string[]> GetApplications()
+        {
+            return applicationService.GetAll();
+        }
+
+        [HttpGet]
+        public Task<GraphDataDto> GetData(string applicationName)
+        {
+            return graphService.GetData(applicationName);
+        }
+    }
+}
